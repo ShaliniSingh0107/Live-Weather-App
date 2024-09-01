03906d5615827f8d9aa7f533603113624963190d
shalinisingh <shalinisingh9308@gmail.com>
2024-09-01 22:19:38 +0530

    i

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "6fe08feca9edfd806443a748443ef34a",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
